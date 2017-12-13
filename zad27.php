e418d8e3b1680f3b0e4561fbbe3380c400190bcf
Alen Šimec <alen@eburza.hr>
2017-12-13 11:29:37 +0100

    Add files via upload

---
+++ zad27.php
@@ -0,0 +1,11 @@
+<?php
+	function stanjeProizvoda($kolicina) 	{
+    if($kolicina>=1) {
+        echo "Proizvod na stanju."; 
+		if ($kolicina<=5) { echo "Treba naručiti proizvod."; }
+	}
+     else if($kolicina == 0) {
+        echo "Proizvod nije na stanju!"; }
+     }
+    stanjeProizvoda(1);
+?>
